ed9b24e07ea3cdcac2931b2a754106bfe7bbca27
Franck Gaudin <franck.gaudin@bnc.ca>
2019-12-05 10:06:55 -0500

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from 'next/head';
+
+import './index.scss';
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    Welcome !
+  </div>
+);
+
+export default Home;

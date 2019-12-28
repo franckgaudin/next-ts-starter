fb7d3b7920f3dd1c13638ec8e1e3bc19104ab986
Franck Gaudin <franck@franckgaudin.com>
2019-12-27 21:46:41 -0500

    Configure TinaCMS in App

---
+++ pages/_app.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import App from 'next/app';
+import { Tina, TinaCMS } from 'tinacms';
+
+class MyApp extends App {
+  constructor() {
+    super();
+    // initialize the cms
+    this.cms = new TinaCMS();
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    // Wrap the page with Tina, provide the cms
+    return (
+      <Tina cms={this.cms}>
+        <Component {...pageProps} />
+      </Tina>
+    );
+  }
+}
+
+export default MyApp;

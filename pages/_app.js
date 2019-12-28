fe663542c4fa86a993612c1fed7a53bbc1d09ccb
Franck Gaudin <franck@franckgaudin.com>
2019-12-27 21:53:00 -0500

    Connecting Back & Front

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import App from 'next/app';
 import { Tina, TinaCMS } from 'tinacms';
+// import the git client
+import { GitClient } from '@tinacms/git-client';
 
 class MyApp extends App {
   constructor() {
     super();
     // initialize the cms
     this.cms = new TinaCMS();
+    // create the client
+    const client = new GitClient('http://localhost:3000/___tina');
+    // register client with the cms
+    this.cms.registerApi('git', client);
   }
 
   render() {
